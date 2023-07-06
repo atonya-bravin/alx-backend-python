b62fcb208f8334d27b987b4fa0bbc1b688f9b650
= <=>
2023-07-06 20:10:02 +0000

    Contains the floor function that returns the floor of a number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,24 @@
+#!usr/bin/env python3
+"""
+    This module contains a method floor that
+    takes in a float number and returns its floor
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+        This function calculates and returns the floor of a number
+
+        Args:
+            n: the floating point number
+
+        Return:
+            Returns the floor of the float number
+
+        Raises:
+            No error checks
+    """
+
+    return (math.floor(n))
