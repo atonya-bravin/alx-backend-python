d74133388569b26cb1c2da60ecac8d361502fd2f
= <=>
2023-07-06 20:21:19 +0000

    Contains a function that returns the string representation of a float number

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    This module contains a method to_str
+    that takes in a float and returns ints string rep.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        This function takes in a float number
+        and returns its string representation.
+
+        Args:
+            n: the float number
+
+        Returns:
+            A string representation of the float number
+
+        Raises:
+            No error checks
+    """
+
+    return (str(n))
