cd33407f7fa6628636b2965b9111b25addbc10c2
= <=>
2023-07-06 18:26:24 +0000

    add() function documentation included

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
-# This is a function that takes in two floats and returns their sum
-
 
 def add(a: float, b: float) -> float:
+    """This is a function that takes in two floats and returns their sum
+
+    Args:
+        a: the first number
+        b: the second number
+
+    Returns:
+        sum of the two numbers
+
+    Raises:
+        No error checks
+    """
+
     return (a + b)
