cd39b5134e0c7cfa2611889fda787ac66080c227
= <=>
2023-07-06 20:00:41 +0000

    Contains a function that takes in two strings and returns the result of their concatination

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    This module contains a method concat
+    that takes in two strings and returns their concatination
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        This function takes in two strings and
+        returns the result of their concatination
+
+        Args:
+            str1: the first string
+            str2: the second string
+
+        Returns:
+            the result of the concatination of the two strings
+
+        Raises:
+            No error checks
+    """
+
+    return (str1 + str2)
