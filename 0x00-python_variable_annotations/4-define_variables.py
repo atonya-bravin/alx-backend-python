2db286bc8d82387d84236bd20e4f7c40f090b100
= <=>
2023-07-06 20:28:49 +0000

    Module that uses anotation to define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    This is a module that anotates variables with types
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
