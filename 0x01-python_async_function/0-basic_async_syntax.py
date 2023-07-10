6aa60ed8c40d9f06928605984dfebd0ba48e275a
= <=>
2023-07-10 19:38:55 +0000

    Contains a function that returns a value passed to it in a random number of seconds between two numbers

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python3
+"""
+This module contains a method wait_random that returns a value
+passed to it in a random number of seconds between two numbers.
+"""
+
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    """
+    This function returns the max_delay passed to it within a
+    given random time between 0 and max_delay
+    """
+
+    delay = random.uniform(0.0, max_delay)
+    await asyncio.sleep(delay)
+    return (delay)
