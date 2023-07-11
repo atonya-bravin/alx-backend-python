5828f3381052c56a6072065aaeaa7dd50728be3d
= <=>
2023-07-11 08:41:54 +0000

    an async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay. You will spawn wait_random n times with the specified max_delay.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,9 +9,16 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """returns a list of completed n tasks"""
+
+    # creation of a list of concurrent tasks which start at the same time
     concurrent_tasks = [asyncio.create_task(wait_random(max_delay))
                         for _ in range(n)]
     completed_tasks = []
+
+    """Ensure that tasks are added in the complete tasks list according to
+    their completition time. This means that the order will be ascending
+    'first task to complete is first while the others follow in that order'
+    """
     for task in asyncio.as_completed(concurrent_tasks):
         completed_tasks.append(await task)
     return completed_tasks
