f79e063470470f351974bd94d912ca4f02c9982d
= <=>
2023-07-11 11:58:40 +0000

    coroutine that will loop 10 times, each time asynchronously wait 1 second, then yield a random number between 0 and 10.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,9 +8,10 @@ then yield a random number between 0 and 10. Use the random module.
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     loops 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10.
