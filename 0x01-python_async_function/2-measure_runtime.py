126f8754fd77272c7a32e18a2eb590c0cf2a9930
= <=>
2023-07-11 09:07:55 +0000

    Creates a measure_time function with integers n and max_delay as arguments that measures the total execution time for wait_n(n, max_delay), and returns total_time / n

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" This module contains a function that mesures runtime """
+
+import asyncio
+from time import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    returns execution time of async function
+    divided by the number of tasks from said func
+    """
+    start_time = time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time() - start_time
+    return end / n
